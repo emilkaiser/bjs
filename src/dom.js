6be972325782bab305e4f130df7e2c5b2999c9e1
Jesper Petersson <jesper@jpetersson.se>
2014-03-20 08:32:30 +0100

    Added a way to inject inline scripts using bjs.dom

--- src/dom.js
+++ src/dom.js
@@ -5,41 +5,67 @@
     root.bjs = {};
   }
 
+  var imageAndIframeOptions = function (src, options) {
+    options = options || {};
+    options.src = options.src || src;
+    options.width = options.width || 1;
+    options.height = options.height || 1;
+    options.style = options.style || {};
+    options.style.position = 'absolute';
+    options.style.left = '-2000px';
+    options.style.top = '-2000px';
+    return options;
+  };
+
+  var isHash = function (obj) {
+    return !!(obj && (typeof obj === 'object'));
+  };
+
+  var merge = function (dst, src) {
+    for (var key in src) {
+      if (src.hasOwnProperty(key)) {
+        if (isHash(src[key])) {
+          dst[key] = dst[key] || {};
+          merge(dst[key], src[key]);
+        }
+        else {
+          dst[key] = src[key];
+        }
+      }
+    }
+  };
+
   root.bjs.dom = (function (d) {
     var el, id;
-    function inject (src, type, options) {
+    function inject (hashSeed, type, options) {
       options = options || {};
-      id = 'bjsdl_' + root.bjs.string.hash(src);
+      id = 'bjsdl_' + root.bjs.string.hash(hashSeed);
       if (d.getElementById(id)) {
         return;
       }
       el = d.createElement(type);
-      el.src = src;
       el.id = id;
-      if (type !== 'script') {
-        el.width = '1';
-        el.height = '1';
-        el.style.position = 'absolute';
-        el.style.left = '-2000px';
-        el.style.top = '-2000px';
-      } else {
-        el.async = true;
-      }
-      for (var key in options) {
-        if (options.hasOwnProperty(key)) {
-          el[key] = options[key];
-        }
-      }
+      merge(el, options);
       d.body.appendChild(el);
     }
     return {
       script: function (src, options) {
+        options = options || {};
+        options.async = options.async === undefined ? true : options.async;
+        options.src = options.src || src;
         inject(src, 'script', options);
       },
+      inlineScript: function (content, options) {
+        options = options || {};
+        options.innerHTML = options.innerHTML || content;
+        inject(options.innerHTML, 'script', options);
+      },
       iframe: function (src, options) {
+        options = imageAndIframeOptions(src, options);
         inject(src, 'iframe', options);
       },
       img: function (src, options) {
+        options = imageAndIframeOptions(src, options);
         inject(src, 'img', options);
       }
     };
